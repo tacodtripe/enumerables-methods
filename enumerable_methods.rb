0b66e78b68909a87750d3456109122670fc39f08
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-07-02 06:35:46 -0600

    fixed requirements from TSE

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -6,8 +6,13 @@ module Enumerable
     while i < size
       if is_a? Hash
         yield(keys[i], self[keys[i]])
+        return self if i == size - 1
       elsif is_a? Array
-        yield(self[i])
+        yield self[i]
+        return self if i == size - 1
+      elsif is_a? Range
+        yield to_a[i]
+        return self if i == size - 1
       end
       i += 1
     end
@@ -20,8 +25,13 @@ module Enumerable
     while i < size
       if is_a? Hash
         yield(keys[i], self[keys[i]])
+        return self if i == size - 1
       elsif is_a? Array
         yield(self[i], i)
+        return self if i == size - 1
+      elsif is_a? Range
+        yield to_a[i], i
+        return self if i == size - 1
       end
       i += 1
     end
@@ -41,73 +51,88 @@ module Enumerable
     end
   end
 
-  def my_all?
-    return_values = []
-    my_each do |n|
-      return_values << yield(n)
-    end
-    if return_values.include?(false)
-      false
-    else
-      true
-    end
-  end
-
-  def my_any?
-    return_values = []
-    my_each do |n|
-      return_values << yield(n)
-    end
-    if return_values.include?(true)
-      true
-    else
-      false
-    end
-  end
-
-  def my_none?
-    return_values = []
-    my_each do |n|
-      return_values << yield(n)
-    end
-    if return_values.include?(true)
-      false
-    else
-      true
-    end
-  end
-
-  def my_count(par = nil)
-    i = 0
-    if par
-      my_each { |n| i += 1 if n == p }
-    elsif !block_given?
-      i = size
-    elsif !par
-      my_each { |n| i += 1 if yield(n) }
-    end
-    i
-  end
-
-  def my_map(proc = nil)
-    arr = []
-    (0..length - 1).each do |i|
-      if proc
-        arr.push(proc.call(self[i]))
+  def my_all?(param = nil)
+    result = true
+    my_each do |value|
+      if block_given?
+        result = false unless yield(value)
+      elsif param.nil?
+        result = false unless value
       else
-        arr.push(yield self[i])
+        result = false unless param === value
       end
     end
-    arr
+    result
   end
 
-  def my_inject(par = nil)
-    memo = par.nil? ? first : par
-    m = par.nil? ? my_count - 1 : my_count
-    last(m).each do |i|
-      memo = yield(memo, i)
+  def my_any?(args = nil)
+    result = false
+    if args.nil? && !block_given?
+      my_each { |x| result = true unless x.nil? || !x }
+    elsif args.nil?
+      my_each { |x| result = true if yield(x) }
+    elsif args.is_a? Regexp
+      my_each { |x| result = true if x.match(args) }
+    elsif args.is_a? Module
+      my_each { |x| result = true if x.is_a?(args) }
+    else
+      my_each { |x| result = true if x == args }
     end
-    memo
+    result
+  end
+
+  def my_none?(args = nil)
+    result = true
+    if args.nil? && !block_given?
+      my_each { |x| result = false if x == true }
+    elsif args.nil?
+      my_each { |x| result = false if yield(x) }
+    elsif args.is_a? Regexp
+      my_each { |x| result = false if x.match(args) }
+    elsif args.is_a? Module
+      my_each { |x| result = false if x.is_a?(args) }
+    else
+      my_each { |x| result = false if x == args }
+    end
+    result
+  end
+
+  def my_count(args = nil)
+    total = 0
+    if args
+      my_each { |i| total += 1 if i == args }
+    elsif !block_given?
+      total = size
+    elsif !args
+      my_each { |i| total += 1 if yield i }
+    end
+    total
+  end
+
+  def my_map(&proc_0)
+    return to_enum(:my_map) unless block_given?
+
+    map = []
+    if !proc_0.nil?
+      my_each { |val| map.push(proc_0.call(val)) }
+    elsif block_given?
+      my_each { |val| map.push(yield(val)) }
+    end
+    map
+  end
+
+  def my_inject(*args)
+    array = is_a?(Range) ? to_a : self
+
+    argument = args[0] if args[0].is_a?(Integer)
+    operator = args[0].is_a?(Symbol) ? args[0] : args[1]
+
+    if operator
+      array.my_each { |i| argument = argument ? argument.send(operator, i) : i }
+      return argument
+    end
+    array.my_each { |i| argument = argument ? yield(argument, i) : i }
+    argument
   end
 end
 
@@ -115,111 +140,10 @@ def multiply_els(array)
   array.my_inject { |memo, n| memo * n }
 end
 
-array_example = [1, 2, 3, 'apple', 'banana', 'orange']
-hash_example = { 1 => 'apple', 2 => 'banana', 3 => 'orange' }
+array = [1, 2, 3, 4, 5]
+range = 0..5
+hash = { a: 1, b: 2, c: 3 }
 
-puts 'Ruby standard each method'
-
-array_example.each do |n|
-  puts n
-end
-
-hash_example.each do |n, v|
-  puts "#{n} for #{v}"
-end
-
-puts "\nmy_each method"
-
-array_example.my_each do |n|
-  puts n
-end
-
-hash_example.my_each do |n, v|
-  puts "#{n} for #{v}"
-end
-
-puts "\nRuby standard each_with_index method"
-
-array_example.each_with_index do |n, i|
-  puts "at index #{i} is #{n}"
-end
-
-puts "\nmy_each_with_index method"
-
-array_example.my_each_with_index do |n, i|
-  puts "at index #{i} is #{n}"
-end
-
-puts "\nRuby standard selec method"
-
-puts(array_example.select { |n| n.is_a? Numeric })
-
-puts(hash_example.select { |k, _v| k > 1 })
-
-puts "\nmy_select method"
-
-puts(array_example.my_select { |n| n.is_a? Numeric })
-
-puts(hash_example.my_select { |k, _v| k > 1 })
-
-puts "\nRuby standard all? method"
-
-puts(array_example.all? { |n| n.is_a? Numeric })
-
-puts "\nmy_all? method"
-
-puts(array_example.my_all? { |n| n.is_a? Numeric })
-
-puts "\nRuby standard any? method"
-
-puts(array_example.any? { |n| n.is_a? Numeric })
-
-puts "\nmy_any? method"
-
-puts(array_example.my_any? { |n| n.is_a? Numeric })
-
-puts "\nRuby standard none? method"
-
-puts(array_example.none? { |n| n.is_a? Numeric })
-
-puts "\nmy_none? method"
-
-puts(array_example.my_none? { |n| n.is_a? Numeric })
-
-puts "\nRuby standard count method"
-
-puts(array_example.count)
-puts(array_example.count('apple'))
-puts(array_example.count { |n| n.is_a? Numeric })
-
-puts "\nmy_count? method"
-
-puts(array_example.my_count)
-puts(array_example.my_count('apple'))
-puts(array_example.my_count { |n| n.is_a? Numeric })
-
-puts "\nRuby standard map method"
-
-puts(array_example.map { |n| n * 2 })
-
-puts "\nmy_map method"
-
-puts(array_example.my_map { |n| n * 2 })
-
-puts "\nRuby standard inject method"
-
-puts((5..10).inject { |sum, n| sum + n })
-
-puts "\nmy_inject method"
-
-puts((5..10).my_inject { |sum, n| sum + n })
-
-puts "\nmultiply method"
-
-puts(multiply_els([2, 4, 5]))
-
-proc_example = proc { |n| n * 2 }
-
-puts "\nmy_map with proc"
-
-puts(array_example.my_map(&proc_example))
+p array.all? { |n| n > 3 }
+puts ''
+p array.my_all? { |n| n > 3 }
